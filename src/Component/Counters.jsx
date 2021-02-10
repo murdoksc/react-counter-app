fda225bc9cc232d357da8123847ef9544df40247
carlos ivan gutierrez correa <gosu2424@hotmail.com>
2021-02-09 20:43:53 -0600

    stage file

--- src/Component/Counters.jsx
+++ src/Component/Counters.jsx
@@ -1,23 +1,44 @@
 import React, { Component } from "react";
+import reactDom from "react-dom";
 import Counter from "./Counter";
 
 class Counters extends Component {
   render() {
-    const { onReset, counters, onDelete, onIncrement } = this.props;
+    const {
+      onReset,
+      counters,
+      onDelete,
+      onDecrement,
+      onIncrement,
+    } = this.props;
     return (
-      <div>
-        <button className="btn btn-primary m-2" onClick={onReset}>
-          RESET COUNTERS
-        </button>
-        {counters.map((m) => (
-          <Counter
-            key={m.id}
-            counter={m}
-            onDelete={onDelete}
-            onIncrement={onIncrement}
-          ></Counter>
-        ))}
-      </div>
+      <React.Fragment>
+        <table className="table">
+          <tbody>
+            <tr>
+              <td>
+                <button className="btn btn-primary m-2" onClick={onReset}>
+                  RESET
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                {" "}
+                {counters.map((m) => (
+                  <Counter
+                    key={m.id}
+                    counter={m}
+                    onDelete={onDelete}
+                    onDecrement={onDecrement}
+                    onIncrement={onIncrement}
+                  ></Counter>
+                ))}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
